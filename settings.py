1eea5ccfc954aa3a21dec41406a7667477967630
Pier4413 <pierre.delmas@delmasweb.net>
2022-02-22 21:15:12 +0100

    More pythonic version

--- settings.py
+++ settings.py
@@ -1,6 +1,4 @@
 from configparser import ConfigParser
-from re import S
-from xmlrpc.client import boolean
 
 class Settings():
     """
@@ -38,92 +36,92 @@ class Settings():
         else:
             Settings.__instance = self
 
-    def loadSettings(self, settingsFile : str = "./settings.ini") -> None:
+    def loadSettings(self, settings_file : str = "./settings.ini") -> None:
         """
             This function load the settings from a file
 
             :param settingsFile: Optional; Default : ./settings.ini; The settings file path (relative or absolute)
             :type settingsFile: str
         """
-        self.__fileName = settingsFile
+        self.__fileName = settings_file
         self.__configur = ConfigParser()
-        self.__configur.read(settingsFile)
+        self.__configur.read(settings_file)
 
-    def get(self, section : str, option : str, defaultValue : str) -> None:
+    def get(self, section : str, option : str, default_value : str) -> None:
         """
-            This function returns the value of the option in the section if exists or defaultValue otherwise
+            This function returns the value of the option in the section if exists or default_value otherwise
 
             :param section: The section in the ini file
             :type section: str
             :param option: The option in the ini file
             :type option: str
-            :param defaultValue: The default value
-            :type defaultValue: str
+            :param default_value: The default value
+            :type default_value: str
         """
         if(self.__configur.has_section(section) == True):
             if(self.__configur.has_option(section, option) == True):
                 return self.__configur.get(section, option)
             else:
-                return defaultValue
+                return default_value
         else:
-            return defaultValue
+            return default_value
 
-    def getint(self, section : str, option : str, defaultValue : int) -> None:
+    def getint(self, section : str, option : str, default_value : int) -> None:
         """
-            This function returns the value of the option in the section if exists or defaultValue otherwise
+            This function returns the value of the option in the section if exists or default_value otherwise
 
             :param section: The section in the ini file
             :type section: str
             :param option: The option in the ini file
             :type option: str
-            :param defaultValue: The default value
-            :type defaultValue: int
+            :param default_value: The default value
+            :type default_value: int
         """
         if(self.__configur.has_section(section) == True):
             if(self.__configur.has_option(section, option) == True):
                 return self.__configur.getint(section, option)
             else:
-                return defaultValue
+                return default_value
         else:
-            return defaultValue
+            return default_value
 
-    def getboolean(self, section : str, option : str, defaultValue : bool) -> None:
+    def getboolean(self, section : str, option : str, default_value : bool) -> None:
         """
-            This function returns the value of the option in the section if exists or defaultValue otherwise
+            This function returns the value of the option in the section if exists or default_value otherwise
 
             :param section: The section in the ini file
             :type section: str
             :param option: The option in the ini file
             :type option: str
-            :param defaultValue: The default value
-            :type defaultValue: bool
+            :param default_value: The default value
+            :type default_value: bool
         """
         if(self.__configur.has_section(section) == True):
             if(self.__configur.has_option(section, option) == True):
                 return self.__configur.getboolean(section, option)
             else:
-                return defaultValue
+                return default_value
         else:
-            return defaultValue
+            return default_value
 
-    def getfloat(self, section : str, option : str, defaultValue : float) -> None:
+    def getfloat(self, section : str, option : str, default_value : float) -> None:
         """
-            This function returns the value of the option in the section if exists or defaultValue otherwise
+            This function returns the value of the option in the section if exists or default_value otherwise
 
             :param section: The section in the ini file
             :type section: str
             :param option: The option in the ini file
             :type option: str
-            :param defaultValue: The default value
-            :type defaultValue: float
+            :param default_value: The default value
+            :type default_value: float
         """
         if(self.__configur.has_section(section) == True):
             if(self.__configur.has_option(section, option) == True):
                 return self.__configur.getfloat(section, option)
             else:
-                return defaultValue
+                return default_value
         else:
-            return defaultValue
+            return default_value
 
     def set(self, section : str, option : str, value : str) -> None:
         """
