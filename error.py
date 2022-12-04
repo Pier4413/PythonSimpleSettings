5fcb3fd72cc6663b76e5ca5bb321d399a3a690cd
Pier4413 <pierre.delmas@delmasweb.net>
2022-12-04 21:56:05 +0100

    Update the error to have better exception handling

--- error.py
+++ error.py
@@ -1,2 +1,7 @@
 class SettingsError(Exception):
-  pass
+  """
+    Exception handler for settings related problem
+  """
+  def __init__(self, message : str = "Settings general error") -> None:
+    self.message = message
+    super.__init__(self.message)
