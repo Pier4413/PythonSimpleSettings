d02ea03e6046a468dc68bde4a04edf1c54f87f60
Pier4413 <pierre.delmas@delmasweb.net>
2023-12-22 15:05:10 +0100

    Add the module creation and upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='settings',
+    packages=['settings'],
+    description='A simple wrapper for configparser',
+    version='1.0.0',  # updated
+    url='',
+    author='Panda',
+    author_email='panda@delmasweb.net',
+    install_requires=[
+        'configparser'
+    ],
+    keywords=['settings']
+)
