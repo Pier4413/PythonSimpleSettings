a921d3ad67720b7d5705421653a02a0b2b4649f8
Pier4413 <pierre.delmas@delmasweb.net>
2023-12-30 18:07:00 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='settings',
     packages=['settings'],
     description='A simple wrapper for configparser',
-    version='1.0.0',  # updated
-    url='',
+    version='1.0.1',  # updated
+    url='https://github.com/Pier4413/PythonSimpleSettings',
     author='Panda',
     author_email='panda@delmasweb.net',
     install_requires=[
