071033f11b5242a9d52a6b05146b4f6039ee1521
Pier4413 <pierre.delmas@delmasweb.net>
2025-05-20 18:47:40 +0000

    New paradigm for metadata

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,11 +1,25 @@
 
 from configparser import ConfigParser
 
+from theophanie_models.life import Life as LifeMessage
+
 __authors__ = ["Panda <panda@delmasweb.net>"]
 __short_licence_name__ = "No licence"
 __long_licence_text__ = ""
 __version__ = "1.0.0"
 __name__ = "Settings"
+
+def create_life_message() -> LifeMessage:
+
+  """
+    Create the life message in the format of models
+  """
+  authors_part = LifeMessage(text="Authors", value=str(__authors__))
+  licence_part = LifeMessage(text="Licence", value=__short_licence_name__)
+  version_part = LifeMessage(text="Version", value=__version__)
+  life_message = LifeMessage(text=__name__, childs=[version_part, authors_part, licence_part])
+
+  return life_message
 
 class Settings():
     """
