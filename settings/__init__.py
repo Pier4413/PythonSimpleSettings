0f19d6832b1b2af1d53485344848c61c2c9df9ca
Pier4413 <pierre.delmas@delmasweb.net>
2025-05-20 20:09:17 +0000

    Add the types for the life message

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,7 +1,7 @@
 
 from configparser import ConfigParser
 
-from theophanie_models.life import Life as LifeMessage
+from theophanie_models.life import Life as LifeMessage, LifeType
 
 __authors__ = ["Panda <panda@delmasweb.net>"]
 __short_licence_name__ = "No licence"
@@ -14,10 +14,10 @@ def create_life_message() -> LifeMessage:
   """
     Create the life message in the format of models
   """
-  authors_part = LifeMessage(text="Authors", value=str(__authors__))
-  licence_part = LifeMessage(text="Licence", value=__short_licence_name__)
-  version_part = LifeMessage(text="Version", value=__version__)
-  life_message = LifeMessage(text=__name__, childs=[version_part, authors_part, licence_part])
+  authors_part = LifeMessage(text="Authors", value=str(__authors__), type=LifeType.INFO)
+  licence_part = LifeMessage(text="Licence", value=__short_licence_name__, type=LifeType.INFO)
+  version_part = LifeMessage(text="Version", value=__version__, type=LifeType.INFO)
+  life_message = LifeMessage(text=__name__, childs=[version_part, authors_part, licence_part], type=LifeType.INFO)
 
   return life_message
 
